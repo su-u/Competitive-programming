c29422bb680153c2ed2fa7a808a3da8c87cf302a
すー <aaaiiiuuu35@gmail.com>
2019-03-03 22:13:32 +0900

    ABC001A

---
+++ ABC/ABC001A/ABC001A/ABC001A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC001A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(int.Parse(Console.ReadLine()) - int.Parse(Console.ReadLine()));
+        }
+    }
+}
