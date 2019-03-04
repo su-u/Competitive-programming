895e96c66cd594915ee03af20f753f106f6dedbd
すー <aaaiiiuuu35@gmail.com>
2019-03-04 13:29:00 +0900

    d

--- ABC/ABC001/ABC001C/ABC001C/Program.cs
+++ ABC/ABC001/ABC001C/ABC001C/Program.cs
@@ -3,59 +3,55 @@
 namespace ABC001C {
     class Program {
         static void Main(string[] args) {
-            //Program.Main1();
-            for(int i = 0;i < 3600; i += 10) {
-                Console.WriteLine($"{i}:{((i * 10 + 1125 / 2250) % 16)}");
-            }
+            Program.Main1();
         }
 
 
-        static void Main1() { 
+        static void Main1() {
             var s = Console.ReadLine();
             var ss = s.Split(' ');
             var deg = int.Parse(ss[0]);
             var degs = string.Empty;
 
-            var b = ((deg * 10 + 1125 / 2250) % 16);
+            var b = ((deg * 10 + 1125) / 2250) % 16;
 
             string[] a = { "N", "NNE", "NE", "EME", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WMW", "NW", "NNW" };
 
             degs = a[b];
 
-            var dis = double.Parse(ss[1]) / 60.0;
-            Console.WriteLine(dis);
-            dis = Math.Round(dis, 1, MidpointRounding.AwayFromZero);
-            Console.WriteLine(dis);
+            var dis = double.Parse(ss[1]);
+            //Console.WriteLine(dis);
             var diss = 0;
-            if(dis <= 0.2) {
+            if(dis <= 12) {
                 diss = 0;
                 degs = "C";
-            }else if(dis <= 1.5) {
+            }else if(dis <= 90) {
                 diss = 1;
-            }else if(dis <= 3.3) {
+            }else if(dis <= 198) {
                 diss = 2;
-            }else if(dis <= 5.4) {
+            }else if(dis <= 324) {
                 diss = 3;
-            }else if(dis <= 7.9) {
+            }else if(dis <= 474) {
                 diss = 4;
-            }else if(dis <= 10.7) {
+            }else if(dis <= 642) {
                 diss = 5;
-            }else if(dis <= 13.8) {
+            }else if(dis <= 828) {
                 diss = 6;
-            }else if(dis <= 17.1) {
+            }else if(dis <= 1026) {
                 diss = 7;
-            }else if(dis <= 20.7) {
+            }else if(dis <= 1242) {
                 diss = 8;
-            }else if(dis <= 24.4) {
+            }else if(dis <= 1452) {
                 diss = 9;
-            }else if(dis <= 28.4) {
+            }else if(dis <= 1704) {
                 diss = 10;
-            }else if(dis <= 32.6) {
+            }else if(dis <= 1956) {
                 diss = 11;
             } else {
                 diss = 12;
             }
-
+            dis = Math.Round(dis / 60.0, 1, MidpointRounding.AwayFromZero);
+            //Console.WriteLine(dis);
 
             Console.WriteLine($"{degs} {diss}");
         }
