9f4fb6349db4fdf0e5aef20c8790f824eb738227
すー <aaaiiiuuu35@gmail.com>
2019-03-07 17:20:24 +0900

    add ABC001D

---
+++ ABC/ABC001/ABC001D/ABC001D/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC001D {
+    class Program {
+        static void Main(string[] args) {
+            var n = Int32.Parse(Console.ReadLine());
+            var s = Console.ReadLine();
+
+        }
+
+        static void Main1(string s) {
+
+        }
+    }
+}
