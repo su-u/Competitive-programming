c1d60c7392d2bcdf73ad43809ff58eef4ca71371
すー <aaaiiiuuu35@gmail.com>
2019-03-13 21:34:10 +0900

    Update Program.cs

--- ABC/ABC002/ABC002A/ABC002A/Program.cs
+++ ABC/ABC002/ABC002A/ABC002A/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace ABC002A {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine(Console.ReadLine().Split().Select(Int32.Parse).OrderByDescending(x => x).ToArray()[0]);
+
         }
     }
 }
