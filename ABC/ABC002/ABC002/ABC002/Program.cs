4c8e275eb33f676a919f2dbcafc4600bcb6bf0ac
すー <aaaiiiuuu35@gmail.com>
2019-04-26 17:39:38 +0900

    add ABC002 & replace

---
+++ ABC/ABC002/ABC002/ABC002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- ABC/ABC002/ABC002/ABC002A/Program.cs
+++ ABC/ABC002/ABC002/ABC002A/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ABC002A {
-    class Program {
-        static void Main(string[] args) {
+namespace ABC002A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             Console.WriteLine(Console.ReadLine().Split().Select(Int32.Parse).OrderByDescending(x => x).ToArray()[0]);
 
         }
