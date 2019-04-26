bd03ef27542ba97c9d236b399f795b3eae12b4c9
すー <aaaiiiuuu35@gmail.com>
2019-04-26 19:23:02 +0900

    Add ABC002D

---
+++ ABC/ABC002/ABC002/ABC002D/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ABC002C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            P("");
+        }
+
+        static void P(String s)
+        {
+            Int32[] x = Console.ReadLine().Split().Select(a => Int32.Parse(s)).ToArray();
+            Int32[] y = new Int32[x[1]]();
+
+            for(int i = 0;i < x[1]; ++i)
+            {
+
+            }
+
+        }
+    }
+}
