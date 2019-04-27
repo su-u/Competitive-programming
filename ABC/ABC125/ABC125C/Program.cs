0255955a79b3153879ac4e45dbac67f8a5729108
すー <aaaiiiuuu35@gmail.com>
2019-04-27 22:35:28 +0900

    not c

--- ABC/ABC125/ABC125C/Program.cs
+++ ABC/ABC125/ABC125C/Program.cs
@@ -12,12 +12,59 @@ namespace ABC125C
         static void Main(string[] args)
         {
 
-            Int32[] x = Console.ReadLine().Split().Select(s => Int32.Parse(s)).ToArray();
+            Int32 n = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine(x[1] / x[0]);
+            Int32[] v = Console.ReadLine().Split().Select(s => Int32.Parse(s)).ToArray();
 
 
+            Array.Sort(v);
 
+            Int32 a = 0;
+            int o = 1;
+
+            if (n == 2)
+            {
+                    a = v[0];
+            }
+            else
+            {
+                for (int i = 0; i < n - 1; ++i) {
+                    int cou = 0;
+                    a = Gcd(v[i], v[n - 1]);
+                    if (a == 1)
+                    {
+                        break;
+                    }
+                    for(int j = 0;j < n; ++j)
+                    {
+                        if (v[j] % a == 0) cou++;
+                    }
+                    if (cou >= 2)
+                    {
+                        o++;
+                        break;
+                    }
+                }
+            }
+
+
+            Console.WriteLine(a);
+
+            
+
+        }
+        public static int Gcd(int a, int b)
+        {
+            if (a < b)
+                // 引数を入替えて自分を呼び出す
+                return Gcd(b, a);
+            while (b != 0)
+            {
+                var remainder = a % b;
+                a = b;
+                b = remainder;
+            }
+            return a;
         }
     }
 }
