b29e8edb3e319787c44869f9c022ad5a85c0a16f
すー <aaaiiiuuu35@gmail.com>
2019-04-27 21:00:00 +0900

    add 125C

---
+++ ABC/ABC125/ABC125B/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ABC125B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Double[] x = Console.ReadLine().Split().Select(s => Double.Parse(s)).ToArray();
+
+            Console.WriteLine(Math.Abs(((x[0] - x[4]) * (x[3] - x[5]) - (x[2] - x[4]) * (x[1] - x[5])) * 0.5));
+
+
+
+        }
+    }
+}
