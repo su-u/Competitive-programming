3d52723593d1e746b49278cfa7c66625adaa38d6
すー <aaaiiiuuu35@gmail.com>
2019-04-27 20:57:38 +0900

    add 125A

---
+++ ABC/ABC125/ABC125A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC125A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
