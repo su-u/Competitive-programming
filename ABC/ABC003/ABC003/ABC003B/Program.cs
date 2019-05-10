e47fe6d4f8c9d95bba024351a780035864e36287
すー <s1732087AP@s.chibakoudai.jp>
2019-05-10 11:48:58 +0900

    B
    B

--- ABC/ABC003/ABC003/ABC003B/Program.cs
+++ ABC/ABC003/ABC003/ABC003B/Program.cs
@@ -8,8 +8,30 @@ namespace ABC003B
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            var change = new Char[]{ 'a', 't', 'c', 'o', 'd', 'e', 'r'};
+            String s = Console.ReadLine();
+            String t = Console.ReadLine();
+
+            Boolean jg = false;
+
+            foreach(Char i in s)
+            {
+                foreach(Char j in t)
+                {
+                    if (i != j) jg = false;
+                    if(i == '@')
+                    {
+                        if(change.Any(n => n == j))
+                        {
+                           
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
