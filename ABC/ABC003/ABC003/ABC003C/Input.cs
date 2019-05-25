90cbd049d2db179b9683dbb3cf08a8dd0db5ba06
すー <aaaiiiuuu35@gmail.com>
2019-05-25 22:38:50 +0900

    add

---
+++ ABC/ABC003/ABC003/ABC003C/Input.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace CPL.Input
+{
+    static class Input
+    {
+        /// <summary>
+        /// 文字列を任意の型にキャストする。
+        /// </summary>
+        /// <typeparam name="T">変換後の型</typeparam>
+        /// <param name="input">変換する文字列</param>
+        /// <returns>キャストされた値</returns>
+        public static T TryParse<T>(this string input)
+        {
+            try
+            {
+                var converter = TypeDescriptor.GetConverter(typeof(T));
+                if (converter != null)
+                {
+                    return (T)converter.ConvertFromString(input);
+                }
+                else
+                {
+                    throw new InvalidCastException("");
+                }
+            }
+            catch
+            {
+                throw new InvalidCastException(typeof(T) + " is not suported.");
+            }
+        }
+
+        /// <summary>
+        /// 文字列を空白で区切り、任意の型にキャストしたリストに変換する。
+        /// </summary>
+        /// <typeparam name="T">変換後の型</typeparam>
+        /// <param name="input">変換する文字列</param>
+        /// <returns>キャストされた値のリスト</returns>
+        public static List<T> SplitTryParseToList<T>(this string input)
+        {
+            return input.Split().Select(n => n.TryParse<T>()).ToList();
+        }
+
+        
+    }
+}
---
+++ ABC/ABC003/ABC003/ABC003C/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CPL.Input;
+
+namespace ABC003C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Console.ReadLine().SplitTryParseToList<Double>();
+            var r = Console.ReadLine().SplitTryParseToList<Double>();
+
+
+        }
+    }
+}
