e34d9f756005d1fa9a7e368e6e69f3382179ea0a
すー <aaaiiiuuu35@gmail.com>
2019-05-27 18:15:12 +0900

    create ABC005

---
+++ ABC/ABC005/ABC005/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
