48cf6f1b89a6cde6b3c340981e15e1deb41c912e
すー <aaaiiiuuu35@gmail.com>
2019-05-27 17:52:08 +0900

    add 004c

---
+++ ABC/ABC004/ABC004C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC004C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
