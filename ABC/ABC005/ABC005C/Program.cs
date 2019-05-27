9bd7a622ff7653e6908a0efc7dd8c8d14209a229
すー <aaaiiiuuu35@gmail.com>
2019-05-27 22:18:28 +0900

    x C

---
+++ ABC/ABC005/ABC005C/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using static System.Math;
+
+
+namespace ABC005C
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = Console.ReadLine().TryParse<int>();
+            var n = Console.ReadLine().TryParse<int>();
+            var a = Console.ReadLine().SplitTryParseToList<int>();
+            var m = Console.ReadLine().TryParse<int>();
+            var b = Console.ReadLine().SplitTryParseToList<int>();
+
+            Console.WriteLine();
+            int rast = 0;
+            String an = "yes";
+            if(n >= m)
+            {
+                foreach(var i in Enumerable.Range(0, b.Count - 1)) {
+                    foreach(var j in Enumerable.Range(rast, a.Count - 1))
+                    {
+                        var abs = Abs(b[i] - a[j]);
+                        if (abs <= t)
+                        {
+                            rast = j + 1;
+                            break;
+                        }else if(b[j] < a[i])
+                        {
+                            an = "no";
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                an = "no";
+            }
+
+            Console.WriteLine(an);
+
+        }
+
+        /// <summary>
+        /// 文字列を任意の型にキャストする。
+        /// </summary>
+        /// <typeparam name="T">変換後の型</typeparam>
+        /// <param name="input">変換する文字列</param>
+        /// <returns>キャストされた値</returns>
+        public static T TryParse<T>(this String input)
+        {
+            try
+            {
+                var converter = TypeDescriptor.GetConverter(typeof(T));
+                if (converter != null)
+                {
+                    return (T)converter.ConvertFromString(input);
+                }
+                else
+                {
+                    throw new InvalidCastException("");
+                }
+            }
+            catch
+            {
+                throw new InvalidCastException(typeof(T) + " is not suported.");
+            }
+        }
+
+        /// <summary>
+        /// 文字列を空白で区切り、任意の型にキャストしたリストに変換する。
+        /// </summary>
+        /// <typeparam name="T">変換後の型</typeparam>
+        /// <param name="input">変換する文字列</param>
+        /// <returns>キャストされた値のリスト</returns>
+        public static List<T> SplitTryParseToList<T>(this String input)
+        {
+            return input.Split().Select(n => n.TryParse<T>()).ToList();
+        }
+    }
+}
