7312500ae1627ca72cd254343d527427bceab719
すー <aaaiiiuuu35@gmail.com>
2019-05-27 17:31:28 +0900

    ABC004A

---
+++ ABC/ABC004/ABC004/ABC004A.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ABC004
+{
+    public static class Class1
+    {
+        public static void ABC004A()
+        {
+            Console.WriteLine(Console.ReadLine().TryParse<Int32>() * 2);
+        }
+
+        public static T TryParse<T>(this string input)
+        {
+            try
+            {
+                var converter = System.ComponentModel.TypeDescriptor.GetConverter(typeof(T));
+                if (converter != null)
+                {
+                    return (T)converter.ConvertFromString(input);
+                }
+                else
+                {
+                    throw new InvalidCastException("");
+                }
+            }
+            catch
+            {
+                throw new InvalidCastException(typeof(T) + " is not suported.");
+            }
+        }
+
+        /// <summary>
+        /// 文字列を空白で区切り、任意の型にキャストしたリストに変換する。
+        /// </summary>
+        /// <typeparam name="T">変換後の型</typeparam>
+        /// <param name="input">変換する文字列</param>
+        /// <returns>キャストされた値のリスト</returns>
+        public static List<T> SplitTryParseToList<T>(this string input)
+        {
+            return input.Split().Select(n => n.TryParse<T>()).ToList();
+        }
+    }
+}
