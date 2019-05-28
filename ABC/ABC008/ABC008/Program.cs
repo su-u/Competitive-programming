7a4d76f2d91ea9ffed39571ded5048b11097476b
すー <s1732087AP@s.chibakoudai.jp>
2019-05-28 13:28:28 +0900

    Add ABC 007

---
+++ ABC/ABC008/ABC008/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace ABC008
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static T TryParse<T>(this String input)
+        {
+            try
+            {
+                var converter = TypeDescriptor.GetConverter(typeof(T));
+                if (converter != null)
+                {
+                    return (T)converter.ConvertFromString(input);
+                }
+                else
+                {
+                    throw new InvalidCastException("");
+                }
+            }
+            catch
+            {
+                throw new InvalidCastException(typeof(T) + " is not suported.");
+            }
+        }
+
+        public static List<T> SplitTryParseToList<T>(this String input)
+        {
+            return input.Split().Select(n => n.TryParse<T>()).ToList();
+        }
+
+        public static List<T> ListSwap<T>(this List<T> list, Int32 index1, Int32 index2)
+        {
+            var t = list[index1];
+            list[index1] = list[index2];
+            list[index2] = t;
+            return list;
+        }
+    }
+}
