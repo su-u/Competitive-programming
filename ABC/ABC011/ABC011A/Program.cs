3a51572582b8c9349fa7989cd9073f86a87205c3
すー <aaaiiiuuu35@gmail.com>
2019-06-01 12:52:50 +0900

    AC ABC011A

--- ABC/ABC011/ABC011A/Program.cs
+++ ABC/ABC011/ABC011A/Program.cs
@@ -11,7 +11,11 @@ namespace ABC011A
     {
         static void Main(string[] args)
         {
+            int[] a = new[] {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1};
 
+            var n = ReadLine().TryParse<int>();
+
+            WriteLine(a[n]);
         }
 
         public static T TryParse<T>(this String input)
