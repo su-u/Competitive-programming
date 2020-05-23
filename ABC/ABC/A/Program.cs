5be149f1ebef21ce45c537b58536dad1e3b14fb4
su- <aaaiiiuuu35@gmail.com>
2020-05-24 00:02:38 +0900

    template

---
+++ ABC/ABC/A/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace A
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ABC/ABC/C/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace C
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
